15527da568b27259846847a8bea775fb33e0bccd
zedoax <Syther618@gmail.com>
2017-09-01 09:52:32 -0400

    Initial Commit

---
+++ kepler.c
@@ -0,0 +1,15 @@
+/* Program that allows users to input variables from kepler's equation
+ * to calculate missing variables
+ * -------------------------------------------------------------------
+ * @author Elijah Bendinsky
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int args, char[] argc) {
+	
+	return EXIT_SUCCESS;
+}
