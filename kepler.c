a75785091cc79d462095674f42973ecf829d48e8
zedoax <Syther618@gmail.com>
2017-09-08 09:38:36 -0400

    Refactored functions to increase code reusability

--- kepler.c
+++ kepler.c
@@ -34,14 +34,8 @@ float calculate(float* distance, float* axis, float* eccentricity, float* anomal
 	}
 }
 
-int main(int argc, char* argv[]) {
-	char input[128];
-	float distance = 0.0;
-	float axis = 0.0;
-	float eccentricity = 0.0;
-	float anomaly = 0.0;
-	while(1) {
-		fprintf(stdout, "Enter x to clear\n \
+void printterm(distance, axis, eccentricity, anomaly) {
+	fprintf(stdout, " Enter x to clear\n \
 Enter 'c' to calculate missing values\n \
 Enter 'q' to quit\n \
 [1] Distance: %f\n \
@@ -49,6 +43,16 @@ Enter 'q' to quit\n \
 [3] Eccentricity: %f\n \
 [4] True Anomaly: %f\n \
 > ", distance, axis, eccentricity, anomaly);
+}
+
+int runterm() {
+	char input[128];
+	float distance = 0.0;
+	float axis = 0.0;
+	float eccentricity = 0.0;
+	float anomaly = 0.0;
+	while(1) {
+		printterm(distance, axis, eccentricity, anomaly);
 		if(fgets(input, 128, stdin) != NULL) {
 			int x = strtol(input, (char**)NULL, 10);
 			if(x >= 1 && x <= 4) {
@@ -94,5 +98,10 @@ Enter 'q' to quit\n \
 			anomaly = 0.0;
 		}
 	}
+
+}
+
+int main(int argc, char* argv[]) {
+	runterm();
 	return EXIT_FAILURE;
 }
