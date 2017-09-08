577a5a140b867a60acd02ee29305633003d58d21
zedoax <Syther618@gmail.com>
2017-09-08 09:30:58 -0400

    Finished calculation engine 50%

--- kepler.c
+++ kepler.c
@@ -6,6 +6,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14159265
 
 /* Function: calculate
  * ---------------------------------------
@@ -13,7 +16,22 @@
  * and calculates the missing one
  */
 float calculate(float* distance, float* axis, float* eccentricity, float* anomaly) {
-
+	if(*distance == ((float)0.0)) {
+		double x = cos(((double)(*anomaly)));
+		return ((float)(((*axis)*(1-((*eccentricity)*(*eccentricity))))/(1+(*eccentricity)*x)));
+		
+	}
+	else if(*axis == ((float)0.0)) {
+		double x = cos(((double)(*anomaly)));
+		return ((*distance)*(1+((*eccentricity)*x)))/(1-(*eccentricity)*(*eccentricity));
+	}
+	else if(*eccentricity == ((float)0.0)) {
+		// STUB: Need to Calculate Final Equation
+	}
+	else if(*anomaly == ((float)0.0)) {
+		// return arccos(((*distance)/((*axis)*(1-(*eccentricity)*(*eccentricity)))));
+		// STUB: Need to calculate final equation and determine how to use arccos in math.h
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -23,7 +41,9 @@ int main(int argc, char* argv[]) {
 	float eccentricity = 0.0;
 	float anomaly = 0.0;
 	while(1) {
-		fprintf(stdout, "Enter 'q' to quit\n \
+		fprintf(stdout, "Enter x to clear\n \
+Enter 'c' to calculate missing values\n \
+Enter 'q' to quit\n \
 [1] Distance: %f\n \
 [2] Semi-Major Axis: %f\n \
 [3] Eccentricity: %f\n \
@@ -49,14 +69,30 @@ int main(int argc, char* argv[]) {
 				}
 			}
 		}
-		
 		if(input[0] == 'q') {
 			return EXIT_SUCCESS;
 		}
 		else if(input[0] == 'c') {
-			
+			float missed = calculate(&distance, &axis, &eccentricity, &anomaly);
+			if(distance==((float)0.0)) {
+				fprintf(stdout, "The missing distance is: %f\n", missed);
+			}
+			else if(axis==((float)0.0)) {
+				fprintf(stdout, "The missing semi-major axis is: %f\n", missed);
+			}
+			else if(eccentricity==((float)0.0)) {
+				fprintf(stdout, "The missing eccentricity is: %f\n", missed);
+			}
+			else if(anomaly==((float)0.0)) {
+				fprintf(stdout, "The missing anomaly is: %f\n", missed);
+			}
 		}	
-				
+		else if(input[0] == 'x') {
+			distance = 0.0;
+			axis = 0.0;
+			eccentricity = 0.0;
+			anomaly = 0.0;
+		}
 	}
 	return EXIT_FAILURE;
 }
